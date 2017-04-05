5b45556d0c53cc4655ae867963f74683194f4e5c
Zack <zack@zackdembp.cn.ibm.com>
2017-04-05 18:03:18 +0800

    Initial Commit

---
+++ Hello World/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello World
+//
+//  Created by Zack on 4/5/17.
+//  Copyright © 2017 Zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello World
+//
+//  Created by Zack on 4/5/17.
+//  Copyright © 2017 Zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
