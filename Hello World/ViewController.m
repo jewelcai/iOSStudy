df5c3105d38b8cc0067255887970443b8dca43ad
Zack <zack@zackdembp.cn.ibm.com>
2017-04-05 19:21:36 +0800

    1 commit

--- Hello World/ViewController.m
+++ Hello World/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 280, 50)];
+    UIButton *button =[UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame=CGRectMake(0, 0, 200, 50);
+    button.backgroundColor=[UIColor redColor];
+    [button setTitle:@"click" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(changeColor) forControlEvents:UIControlEventTouchUpInside];
+    label.text=@"hello";
+    label.numberOfLines=10;
+    [self.view addSubview:label];
+    [self.view addSubview:button];
+    
+    
+}
+
+-(void) changeColor{
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random()%255/255.0 green:arc4random()%255/255.0 blue:arc4random()%255/255.0 alpha:1];
 }
 
 
